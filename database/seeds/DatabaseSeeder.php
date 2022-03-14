a7ff5cfe70b273c02f34490ee6bf159a0a2be16f
mihi0403 <jdabc0417@gmail.com>
2022-03-14 22:07:22 +0900

    シーダー修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,20 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
-
-        factory(App\User::class, 50)->create()->each(function ($user) {
-        $user->posts()->save(factory(App\Post::class)->make());
-        });
-
         $this->call([
         UsersTableSeeder::class,
-        PostsTableSeeder::class,
-        CommentsTableSeeder::class,
         ]);
     }
 }
